90a8548c0f7eb9c18c2dfc39c0f7aa5478658ce7
Christopher Homberger <christopher.homberger@web.de>
2021-04-11 17:55:41 +0200

    [FEATURE] iorewrite, map multiple paths to one Allows the mcpelauncher to support multiple versions, inclusive internal storage

--- src/iorewrite.h
+++ src/iorewrite.h
@@ -3,8 +3,13 @@
 #include <cstring>
 
 static std::string iorewrite0(const char * path) {
-    if (path && !strcmp(path, shim::from_android_data_dir.data()) && shim::to_android_data_dir.rfind(shim::from_android_data_dir.data(), 0)) {
-        return shim::to_android_data_dir + std::string(path + shim::from_android_data_dir.length());
+    auto len = path ? strlen(path) : 0;
+    if(len > 0) {
+        for(auto&& from : shim::from_android_data_dir) {
+            if (len >= from.size() && !memcmp(path, from.data(), from.size()) && shim::to_android_data_dir.rfind(from.data(), 0)) {
+                return shim::to_android_data_dir + std::string(path + from.length());
+            }
+        }
     }
     return path;
 }
@@ -12,8 +17,13 @@ static std::string iorewrite0(const char * path) {
 template<class T> struct iorewrite1;
 template<class R, class ... arg > struct iorewrite1<R (*) (const char * ,arg...)> {
     template<R(*org)(const char *, arg...)> static R rewrite(const char *path1, arg...a) {
-        if (path1 && !strcmp(path1, shim::from_android_data_dir.data()) && shim::to_android_data_dir.rfind(shim::from_android_data_dir.data(), 0)) {
-            return org((shim::to_android_data_dir + std::string(path1 + shim::from_android_data_dir.length())).data(), a...);
+        auto len = path1 ? strlen(path1) : 0;
+        if(len > 0) {
+            for(auto&& from : shim::from_android_data_dir) {
+                if (len >= from.size() && !memcmp(path1, from.data(), from.size()) && shim::to_android_data_dir.rfind(from.data(), 0)) {
+                    return org((shim::to_android_data_dir + std::string(path1 + from.length())).data(), a...);
+                }
+            }
         }
         return org(path1, a...);
     }
@@ -26,11 +36,17 @@ template<class R, class ... arg > struct iorewrite1<R(const char *,arg...) noexc
 template<class T> struct iorewrite2;
 template<class R, class ... arg > struct iorewrite2<R (*)(const char *,const char *,arg...)> {
     template<R(*org)(const char *,const char *,arg...)> static R rewrite(const char *path1, const char *path2, arg...a) {
-        if (path1 && !memcmp(path1, shim::from_android_data_dir.data(), shim::from_android_data_dir.length()) && shim::to_android_data_dir.rfind(shim::from_android_data_dir.data(), 0)) {
-            return rewrite<org>((shim::to_android_data_dir + std::string(path1 + shim::from_android_data_dir.length())).data(), path2, a...);
-        }
-        if (path2 && !memcmp(path2, shim::from_android_data_dir.data(), shim::from_android_data_dir.length()) && shim::to_android_data_dir.rfind(shim::from_android_data_dir.data(), 0)) {
-            return org(path1, (shim::to_android_data_dir + std::string(path2 + shim::from_android_data_dir.length())).data(), a...);
+        auto len1 = path1 ? strlen(path1) : 0;
+        auto len2 = path2 ? strlen(path2) : 0;
+        if(len1 > 0 || len2 > 0) {
+            for(auto&& from : shim::from_android_data_dir) {
+                if (len1 >= from.length() && !memcmp(path1, from.data(), from.length()) && shim::to_android_data_dir.rfind(from.data(), 0)) {
+                    return rewrite<org>((shim::to_android_data_dir + std::string(path1 + from.length())).data(), path2, a...);
+                }
+                if (len2 >= from.length() && !memcmp(path2, from.data(), from.length()) && shim::to_android_data_dir.rfind(from.data(), 0)) {
+                    return org(path1, (shim::to_android_data_dir + std::string(path2 + from.length())).data(), a...);
+                }
+            }
         }
         return org(path1, path2, a...);
     }
