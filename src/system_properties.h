e82b4fda179fe2e18f878d0798a8129b82d2ce68
Christopher Homberger <christopher.homberger@web.de>
2023-01-09 16:08:25 +0100

    Add missing __system_property_get for 1.19.51.01 (arm64)

--- src/system_properties.h
+++ src/system_properties.h
@@ -6,6 +6,7 @@ typedef struct prop_info prop_info;
 
 namespace shim {
     const prop_info *__system_property_find(const char *name);
+    int __system_property_get(const char *name, char *value);
 
     void add_system_properties_shimmed_symbols(std::vector<shimmed_symbol> &list);
 
