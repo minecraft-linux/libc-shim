f668372d888f2e7a30953affa2ab7ff7e0e5297f
ChristopherHX <christopher.homberger@web.de>
2023-02-20 16:14:29 +0100

    small changes

--- src/network.cpp
+++ src/network.cpp
@@ -1,5 +1,6 @@
 #include "network.h"
 #include "errno.h"
+
 #include <netinet/in.h>
 #include <net/if.h>
 #include <arpa/inet.h>
@@ -34,9 +35,9 @@ bionic::af_family bionic::from_host_af_family(int family) {
         case AF_UNSPEC: return af_family::UNSPEC;
         case AF_INET: return af_family::INET;
         case AF_INET6: return af_family::INET6;
-        #ifndef __APPLE__
+#ifdef AF_NETLINK
         case AF_NETLINK: return af_family::NETLINK;
-        #endif
+#endif
         default: return af_family::UNSPEC;
     }
 }
@@ -46,10 +47,10 @@ int bionic::to_host_af_family(bionic::af_family family) {
         case af_family::UNSPEC: return AF_UNSPEC;
         case af_family::INET: return AF_INET;
         case af_family::INET6: return AF_INET6;
-        #ifndef __APPLE__
+#ifdef AF_NETLINK
         case af_family::NETLINK: return AF_NETLINK;
-        #endif
-        default: return 0;
+#endif
+        default: return AF_UNSPEC;
     }
 }
 
