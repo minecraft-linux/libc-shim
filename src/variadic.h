3e7acd20a7dec1ba39622a6e7e81bc8ac7406487
ChristopherHX <christopher.homberger@web.de>
2023-04-11 19:20:42 +0200

    Fix default LIBC_SHIM_DEFINE_VARIADIC

--- src/variadic.h
+++ src/variadic.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #ifndef LIBC_SHIM_DEFINE_VARIADIC
-#if (defined(__APPLE_) && defined(__aarch64__)) || (defined(_WIN32) && defined(__x86_64__))
+#if (defined(__APPLE__) && defined(__aarch64__)) || (defined(_WIN32) && defined(__x86_64__))
 #define LIBC_SHIM_DEFINE_VARIADIC 0
 #else
 #define LIBC_SHIM_DEFINE_VARIADIC 1
