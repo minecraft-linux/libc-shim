fd2ef065ad041f5f80ab5fe596fafaae1038a26a
ChristopherHX <christopher.homberger@web.de>
2024-10-23 19:55:01 +0200

    Minecraft 1.21.40 (ChromeOS Trial)
    * __system_property_read_callback

--- src/system_properties.cpp
+++ src/system_properties.cpp
@@ -6,6 +6,7 @@
 void shim::add_system_properties_shimmed_symbols(std::vector<shim::shimmed_symbol> &list) {
     list.push_back({"__system_property_find", __system_property_find});
     list.push_back({"__system_property_get", __system_property_get});
+    list.push_back({"__system_property_read_callback", __system_property_read_callback});
 }
 
 const prop_info* shim::__system_property_find(const char *name) {
@@ -15,4 +16,10 @@ const prop_info* shim::__system_property_find(const char *name) {
 int shim::__system_property_get(const char *name, char *value) {
     value[0] = 0;
     return 0;
+}
+
+void shim::__system_property_read_callback(const prop_info *pi, void (*callback)(void *cookie, const char *name, const char *value, uint32_t serial), void *cookie) {
+    if(callback) {
+        callback(cookie, "", "", 0);
+    }
 }
