9005f18eb1ae0c5659e7e0d8c53b79abb9fc92b9
GameParrot <85067619+GameParrot@users.noreply.github.com>
2025-05-08 16:08:58 -0400

    add new 1.21.90.23 functions (#42)

--- src/sched.h
+++ src/sched.h
@@ -4,6 +4,6 @@
 #include <sched.h>
 
 namespace shim {
-    int sched_setaffinity(pid_t pid, size_t set_size, const void* mask);
-
-}
+int sched_setaffinity(pid_t pid, size_t set_size, const void* mask);
+int sched_getaffinity(pid_t pid, size_t cpusetsize, const void* mask);
+}  // namespace shim
